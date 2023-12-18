4777f5e7ffad0bf3742ce2701b7125c76f375fe5
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-18 18:33:30 +0400

    add: Hero component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
+import Hero from '@/components/Hero';
 import StyleGuide from '@/components/StyleGuide';
 
 export default function Home() {
 	return (
 		<main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
-			<StyleGuide />
+			<Hero />
+			{/* <StyleGuide /> */}
+			<div className='h-[4000px]'></div>
 		</main>
 	);
 }
