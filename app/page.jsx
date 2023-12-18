d02454ab3558f82b6d5f2ad354e1e6433a53940b
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-18 17:54:52 +0400

    add: Project Setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import StyleGuide from '@/components/StyleGuide';
+
+export default function Home() {
+	return (
+		<main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
+			<StyleGuide />
+		</main>
+	);
+}
