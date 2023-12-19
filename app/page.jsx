289de5582e9c706c055121b04083dfd673d753d3
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-19 15:16:54 +0400

    add: Header, Nav, NavMobile components

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import StyleGuide from '@/components/StyleGuide';
 
 export default function Home() {
 	return (
 		<main className='w-full max-w-[1440px] bg-white mx-auto overflow-hidden'>
+			<Header />
 			<Hero />
 			{/* <StyleGuide /> */}
 			<div className='h-[4000px]'></div>
